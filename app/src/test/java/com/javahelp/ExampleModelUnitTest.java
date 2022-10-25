ac1e568203da334159ab85c2e4a3db9fb7e08f2c
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-10-24 22:30:03 -0400

    add example unit test for the model from within the app module

---
+++ app/src/test/java/com/javahelp/ExampleModelUnitTest.java
@@ -0,0 +1,19 @@
+package com.javahelp;
+
+import static org.junit.Assert.assertEquals;
+
+import com.javahelp.model.ExampleEntity;
+
+import org.junit.Test;
+
+/**
+ * Example test suite testing elements from the model module within the app module
+ */
+public class ExampleModelUnitTest {
+
+    @Test
+    public void exampleEntity_works() {
+        assertEquals("model in app", new ExampleEntity("model in app").name);
+    }
+
+}
