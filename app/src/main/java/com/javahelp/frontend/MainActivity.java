4894502b16a05068e374e8c98893e74777d63b2a
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-10-24 23:49:33 -0400

    refactor com.javahelp in app module to com.javahelp.frontend

--- app/src/main/java/com/javahelp/frontend/MainActivity.java
+++ app/src/main/java/com/javahelp/frontend/MainActivity.java
@@ -1,8 +1,10 @@
-package com.javahelp;
+package com.javahelp.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.javahelp.R;
 
 public class MainActivity extends AppCompatActivity {
 
