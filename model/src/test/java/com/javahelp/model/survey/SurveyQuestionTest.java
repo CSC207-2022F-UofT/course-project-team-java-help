68af912b0ab759da2b4fffcaa8a19fd6c60e52b0
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-10-31 18:06:00 -0400

    test SurveyQuestion

---
+++ model/src/test/java/com/javahelp/model/survey/SurveyQuestionTest.java
@@ -0,0 +1,61 @@
+package com.javahelp.model.survey;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit tests for {@link SurveyQuestion}
+ */
+public class SurveyQuestionTest {
+
+    SurveyQuestion question, emptyQuestion;
+
+    @Before
+    public void setupQuestion() {
+        emptyQuestion = new SurveyQuestion("This is an example question", new ArrayList<>());
+
+        List<String> responses = new ArrayList<>();
+        responses.add("This is an example response option");
+        responses.add("This is another example response option");
+
+        question = new SurveyQuestion("This is another example question", responses);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void surveyQuestion_throwsIndexOutOfBounds() {
+        emptyQuestion.getAnswer(0);
+    }
+
+    @Test
+    public void surveyQuestion_size() {
+        int emptyLength = 0, nonEmptyLength = 0;
+
+        for (String s : emptyQuestion.getAnswers()) {
+            emptyLength++;
+        }
+
+        for (String s : question.getAnswers()) {
+            nonEmptyLength++;
+        }
+
+        assertEquals(0, emptyLength);
+
+        assertEquals(2, nonEmptyLength);
+    }
+
+    @Test
+    public void surveyQuestion_responseOption() {
+        assertEquals("This is an example response option", question.getAnswer(0));
+    }
+
+    @Test
+    public void surveyQuestion_question() {
+        assertEquals("This is an example question", emptyQuestion.getQuestion());
+    }
+
+}
