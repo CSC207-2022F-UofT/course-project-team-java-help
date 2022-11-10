66e2c5fb2c0f237485fe9e2facdb332a252738a8
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-09 21:15:18 -0500

    add abstract DynamoDB repository class

---
+++ backend/src/main/java/com/javahelp/backend/data/DynamoDBStore.java
@@ -0,0 +1,34 @@
+package com.javahelp.backend.data;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+
+/**
+ * Abstract repository built on AWS DynamoDB
+ */
+abstract class DynamoDBStore {
+
+    private Regions region;
+
+    private AmazonDynamoDB client;
+
+    /**
+     * Creates a new {@link DynamoDBStore}
+     *
+     * @param region {@link Regions} to be used
+     */
+    DynamoDBStore(Regions region) {
+        this.region = region;
+        this.client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+
+    /**
+     * @return {@link AmazonDynamoDB} client
+     */
+    private AmazonDynamoDB getClient() {
+        return client;
+    }
+}
