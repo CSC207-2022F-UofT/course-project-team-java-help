211797f585654b6e52fe3c7d6ef9eaf14ec609ac
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-11 17:03:21 -0500

    Implement user gateway (#60)
    * encode and decode base64 representation of UserPassword from UserPassword
    
    * Implement IUserStore through DynamoDBUserStore
    
    * Test DynamoDBUserStore

--- backend/src/main/java/com/javahelp/backend/data/IUserStore.java
+++ backend/src/main/java/com/javahelp/backend/data/IUserStore.java
@@ -1,38 +1,62 @@
 package com.javahelp.backend.data;
 
+import com.amazonaws.regions.Regions;
 import com.javahelp.model.user.User;
 import com.javahelp.model.user.UserPassword;
 
 /**
  * Interface for interacting with {@link User}s in a database
- *
- * Implementations of create without a specified password should specify some form
- * of default password that is used
  */
-public interface IUserStore extends ICRUDGateway<User, String> {
+public interface IUserStore {
+
+    /**
+     * @return Default implementation of {@link IUserStore}
+     */
+    static IUserStore getDefaultImplementation() {
+        return new DynamoDBUserStore("javahelpBackendUsers", Regions.US_EAST_1);
+    }
 
     /**
      * Creates a {@link User}, then mutates that {@link User}
      * to provide it with its new ID
-     * @param u {@link User} to create
+     *
+     * @param u        {@link User} to create
      * @param password {@link UserPassword} to assign to the new {@link User}
      * @return {@link User} that was created
      */
-    default User create(User u, UserPassword password) {
-        u = create(u);
-        updatePassword(u.getStringID(), password);
-        return u;
-    }
+    User create(User u, UserPassword password);
+
+    /**
+     * @param id {@link String} id of the {@link User} to get
+     * @return {@link User} with the specified id
+     */
+    User read(String id);
+
+    /**
+     * Updates the specified {@link User}
+     *
+     * @param u {@link User} to update
+     */
+    void update(User u);
+
+    /**
+     * Deletes the specified {@link User}
+     *
+     * @param id {@link String} id of the {@link User} to delete
+     */
+    void delete(String id);
 
     /**
      * Updates the password of a {@link User}
-     * @param userId {@link String} ID of the {@link User} to update
+     *
+     * @param userId   {@link String} ID of the {@link User} to update
      * @param password {@link UserPassword} to assign
      */
     void updatePassword(String userId, UserPassword password);
 
     /**
      * Gets the {@link UserPassword} for the specified {@link User}
+     *
      * @param userId {@link String} ID of the {@link User}
      * @return the {@link UserPassword}
      */
---
+++ backend/src/test/java/com/javahelp/backend/data/DynamoDBUserStoreTest.java
@@ -0,0 +1,160 @@
+package com.javahelp.backend.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import com.amazonaws.regions.Regions;
+import com.javahelp.model.user.ClientUserInfo;
+import com.javahelp.model.user.User;
+import com.javahelp.model.user.UserPassword;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * Test suite for {@link DynamoDBUserStore}
+ */
+public class DynamoDBUserStoreTest {
+
+    String tableName = "javahelpBackendUsers";
+
+    Regions regions = Regions.US_EAST_1;
+
+    DynamoDBUserStore db = new DynamoDBUserStore(tableName, regions);
+
+    @Test
+    public void testCreateRead() {
+        UserPassword p = randomUserPassword();
+        ClientUserInfo clientInfo = new ClientUserInfo(
+                "test.client@mail.com",
+                "123  client road",
+                "289375034875093",
+                "Erin",
+                "McDonald");
+        User u = new User("test", clientInfo, "test_user");
+
+        db.create(u, p);
+
+        assertNotEquals("test", u.getStringID());
+
+        User read = db.read(u.getStringID());
+
+        assertEquals(u.getStringID(), read.getStringID());
+        assertEquals(u.getUsername(), read.getUsername());
+        assertEquals(u.getUserInfo().getType(), read.getUserInfo().getType());
+        ClientUserInfo base = (ClientUserInfo) u.getUserInfo(),
+                readUserInfo = (ClientUserInfo) read.getUserInfo();
+
+        assertEquals(base.getEmailAddress(), readUserInfo.getEmailAddress());
+        assertEquals(base.getAddress(), readUserInfo.getAddress());
+        assertEquals(base.getFirstName(), readUserInfo.getFirstName());
+        assertEquals(base.getLastName(), readUserInfo.getLastName());
+
+        db.delete(u.getStringID());
+    }
+
+    @Test
+    public void testUpdate() {
+        UserPassword p = randomUserPassword();
+        ClientUserInfo clientInfo = new ClientUserInfo(
+                "test.client@mail.com",
+                "123  client road",
+                "289375034875093",
+                "Erin",
+                "McDonald");
+        User u = new User("test", clientInfo, "test_user");
+
+        db.create(u, p);
+
+        ClientUserInfo c = (ClientUserInfo) u.getUserInfo();
+        c.setFirstName("john");
+
+        u.setUsername("boo!");
+
+        db.update(u);
+
+        u = db.read(u.getStringID());
+
+        assertEquals("boo!", u.getUsername());
+
+        c = (ClientUserInfo) u.getUserInfo();
+
+        assertEquals("john", c.getFirstName());
+
+        db.delete(u.getStringID());
+    }
+
+    @Test
+    public void testDelete() {
+
+        UserPassword p = randomUserPassword();
+        ClientUserInfo clientInfo = new ClientUserInfo(
+                "test.client@mail.com",
+                "123  client road",
+                "289375034875093",
+                "Erin",
+                "McDonald");
+        User u = new User("test", clientInfo, "test_user");
+
+        db.create(u, p);
+
+        User read = db.read(u.getStringID());
+
+        assertNotNull(read);
+
+        db.delete(u.getStringID());
+
+        User deleted = db.read(u.getStringID());
+
+        assertNull(deleted);
+    }
+
+    @Test
+    public void testPasswordUpdateRead() {
+        UserPassword p = randomUserPassword();
+        ClientUserInfo clientInfo = new ClientUserInfo(
+                "test.client@mail.com",
+                "123  client road",
+                "289375034875093",
+                "Erin",
+                "McDonald");
+        User u = new User("test", clientInfo, "test_user");
+
+        db.create(u, p);
+
+        UserPassword read = db.readPassword(u.getStringID());
+
+        assertEquals(read.getBase64SaltHash(), p.getBase64SaltHash());
+
+        UserPassword newPassword = randomUserPassword();
+
+        db.updatePassword(u.getStringID(), newPassword);
+
+        read = db.readPassword(u.getStringID());
+
+        assertEquals(newPassword.getBase64SaltHash(), read.getBase64SaltHash());
+
+        db.delete(u.getStringID());
+    }
+
+    /**
+     * @return A randomly generated {@link UserPassword}
+     */
+    private static UserPassword randomUserPassword() {
+        Random random = new Random();
+        int saltLength = 1 + random.nextInt(31);
+        int hashLength = 1 + random.nextInt(31);
+
+        byte[] salt = new byte[saltLength];
+        byte[] hash = new byte[hashLength];
+
+        random.nextBytes(salt);
+        random.nextBytes(hash);
+
+        return new UserPassword(salt, hash);
+    }
+
+}
