10c9e288659f7377eea9ad23c4c8e775a5240b51
stevolopolis <stevenluots@gmail.com>
2022-11-18 00:14:29 -0500

    refactored for utilizing parent class methods.

--- backend/src/main/java/com/javahelp/backend/query/VanillaConstraint.java
+++ backend/src/main/java/com/javahelp/backend/query/VanillaConstraint.java
@@ -4,11 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class VanillaConstraint extends Constraint{
-    public VanillaConstraint(String question, Integer query) {
-        super(question, query);
-    }
-
-    public HashMap<String, ArrayList<Integer>> getConstraint() {
-        return super.getConstraint();
+    public VanillaConstraint(String question) {
+        super(question);
     }
 }
