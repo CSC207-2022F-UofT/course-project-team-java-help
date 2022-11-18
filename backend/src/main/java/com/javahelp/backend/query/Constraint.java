d92df3374716db4ad2eedced86998edba119b74e
stevolopolis <stevenluots@gmail.com>
2022-11-18 00:13:13 -0500

    refactored interface and abstract class for Constraints

--- backend/src/main/java/com/javahelp/backend/query/Constraint.java
+++ backend/src/main/java/com/javahelp/backend/query/Constraint.java
@@ -1,22 +1,24 @@
 package com.javahelp.backend.query;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public abstract class Constraint {
+public abstract class Constraint implements IConstraint{
     private String question;
-    private Integer query;
+    private ArrayList<String> queryList = new ArrayList<>();
 
-    public Constraint(String question, Integer query) {
+    public Constraint(String question) {
         this.question = question;
-        this.query = query;
     }
 
-    public HashMap<String, ArrayList<Integer>> getConstraint() {
-        ArrayList<Integer> answerList = new ArrayList<>();
-        answerList.add(this.query);
-        HashMap<String, ArrayList<Integer>> constraint = new HashMap<>();
-        constraint.put(question, answerList);
-        return constraint;
+    public void setConstraint(String query) {
+        this.queryList.add(query);
     }
+
+    public HashMap<String, ArrayList<String>> getConstraint(){
+        HashMap<String, ArrayList<String>> constraint = new HashMap<>();
+        constraint.put(this.question, this.queryList);
+        return constraint;
+    };
 }
