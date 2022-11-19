f758eef94ae8018a95b0c9e040c4a4124263cf23
stevolopolis <stevenluots@gmail.com>
2022-11-19 18:20:29 -0500

    deleted redundant interface for Constraint.

--- backend/src/main/java/com/javahelp/backend/query/Constraint.java
+++ backend/src/main/java/com/javahelp/backend/query/Constraint.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public abstract class Constraint implements IConstraint{
+public abstract class Constraint {
     private String question;
     private ArrayList<String> queryList = new ArrayList<>();
 
