290cb3ad4c9a14d215efdecfc415645b8491561e
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-20 00:00:29 -0500

    Remove unnecessary imports in ClientUserInfoConverterTest

--- model/src/test/java/com/javahelp/model/util/json/ClientUserInfoConverterTest.java
+++ model/src/test/java/com/javahelp/model/util/json/ClientUserInfoConverterTest.java
@@ -2,7 +2,6 @@ package com.javahelp.model.util.json;
 
 import static org.junit.Assert.assertEquals;
 
-import com.javahelp.model.token.Token;
 import com.javahelp.model.user.ClientUserInfo;
 
 import org.junit.Test;
