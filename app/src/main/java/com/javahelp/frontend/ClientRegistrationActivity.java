86bb9dc3fd165e2a8f0981cd06d4f719e44b9c8a
Yuanshu Wang <113310999+yuanshu777@users.noreply.github.com>
2022-11-20 20:22:36 -0500

    viewmodel (#87)

--- app/src/main/java/com/javahelp/frontend/ClientRegistrationActivity.java
+++ app/src/main/java/com/javahelp/frontend/ClientRegistrationActivity.java
@@ -1,15 +1,38 @@
 package com.javahelp.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.internal.ViewUtils;
 import com.javahelp.R;
+import com.javahelp.databinding.ActivityClientRegistrationBinding;
 
 
 public class ClientRegistrationActivity extends AppCompatActivity {
+    ClientRegistrationVm clientRegistrationVm;
+    ActivityClientRegistrationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_client_registration);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_client_registration);
+        clientRegistrationVm = new ViewModelProvider(this).get(ClientRegistrationVm.class);
+        binding.setCdata(clientRegistrationVm);
+        binding.setLifecycleOwner(this);
+
+        binding.signupbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ClientRegistrationActivity.this,
+                        FrontPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/javahelp/frontend/MainActivity.java
+++ app/src/main/java/com/javahelp/frontend/MainActivity.java
@@ -2,59 +2,86 @@ package com.javahelp.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.javahelp.BR;
 import com.javahelp.R;
+import com.javahelp.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button buttonLogin;
-    private  Button buttoncr;
-    private  Button buttonpr;
+    MyViewModel myViewModel;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(this);
 
-        //find the login button by its id
-        buttonLogin = (Button) findViewById(R.id.bot_login);
-        //find the client register button by its id
-        buttoncr = (Button) findViewById(R.id.bot_c_reg);
-        //find the provider register button by its id
-        buttonpr = (Button) findViewById(R.id.bot_p_reg);
 
-        // link the button to the second activity(front page)
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        // link the button to the second acticity(front page)
+        binding.botLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,FrontPageActivity.class);
+                Intent intent = null;
+                if (usernameChecker()) {
+                    intent = new Intent(MainActivity.this,FrontPageActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    String inv ="username invalid";
+                    Toast.makeText(getApplicationContext(), inv , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // link the button to the provider Registeration page
+        binding.botPReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = null;
+                intent = new Intent(MainActivity.this,ProviderRegistrationActivity.class);
                 startActivity(intent);
             }
         });
 
-        // link the button to the second activity(front page)
-        buttonpr.setOnClickListener(new View.OnClickListener() {
+        // link the button to the client Registeration page
+        binding.botCReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ProviderRegistrationActivity.class);
+                Intent intent = null;
+                intent = new Intent(MainActivity.this,ClientRegistrationActivity.class);
                 startActivity(intent);
             }
         });
 
-        // link the button to the second activity(front page)
-        buttoncr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ClientRegistrationActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
+
 
     }
+
+    private boolean usernameChecker(){
+        if(binding.et1.getText().toString().isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+
 
 }
