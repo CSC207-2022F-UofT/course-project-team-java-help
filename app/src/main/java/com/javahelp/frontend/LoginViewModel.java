085092a7a1dc853c0d52cca0c827204ff72f8111
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-21 10:33:08 -0500

    comment LoginViewModel

--- app/src/main/java/com/javahelp/frontend/LoginViewModel.java
+++ app/src/main/java/com/javahelp/frontend/LoginViewModel.java
@@ -1,6 +1,7 @@
 package com.javahelp.frontend;
 
 import android.app.Application;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -24,6 +25,9 @@ import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * {@link AndroidViewModel} for {@link LoginActivity}
+ */
 public class LoginViewModel extends AndroidViewModel implements ILoginOutput {
 
     private MutableLiveData<String> username = new MutableLiveData<>("");
@@ -31,9 +35,13 @@ public class LoginViewModel extends AndroidViewModel implements ILoginOutput {
     private MutableLiveData<Boolean> loggingIn = new MutableLiveData<>(Boolean.FALSE);
     private MutableLiveData<Boolean> staySignedIn = new MutableLiveData<>(Boolean.FALSE);
     private MutableLiveData<Optional<LoginResult>> loginResult = new MutableLiveData<>(Optional.empty());
-    private ILoginInput loginInteractor;
     private ExecutorService executor = Executors.newSingleThreadExecutor();
+    private ILoginInput loginInteractor;
 
+    /**
+     * Default {@link AndroidViewModel} constructor for {@link LoginViewModel}
+     * @param application {@link Application} to use
+     */
     public LoginViewModel(@NonNull Application application) {
         super(application);
         ILoginDataAccess login = LambdaLoginDataAccess.getInstance();
@@ -42,46 +50,90 @@ public class LoginViewModel extends AndroidViewModel implements ILoginOutput {
         loginInteractor = new LoginInteractor(this, salt, login, hasher);
     }
 
+    /**
+     * Sets the username for this {@link LoginViewModel}
+     * @param username {@link String} username
+     */
     public void setUsername(String username) {
         this.username.setValue(username);
     }
 
+    /**
+     * Sets the password for this {@link LoginViewModel}
+     * @param password {@link String} password to use
+     */
     public void setPassword(String password) {
         this.password.setValue(password);
     }
 
+    /**
+     * Sets whether this {@link LoginViewModel} represents a {@link View} that
+     * is currently trying to log in
+     * @param b whether currently trying to log in
+     */
     public void setLoggingIn(boolean b) {
         loggingIn.setValue(b);
     }
 
+    /**
+     * Sets whether the current login form is specifying to stay logged in long term
+     * @param b whether to stay logged in
+     */
     public void setStaySignedIn(boolean b) {
         staySignedIn.setValue(b);
     }
 
+    /**
+     * Sets the login error
+     * @param s {@link String} login error to set
+     */
     public void setLoginError(String s) {
         loginResult.setValue(Optional.of(new LoginResult(s)));
     }
 
+    /**
+     *
+     * @return whether the user should stay signed in
+     */
     public MutableLiveData<Boolean> shouldStaySignedIn() {
         return staySignedIn;
     }
 
+    /**
+     *
+     * @return whether there is currently a login attempt going on
+     */
     public MutableLiveData<Boolean> isLoggingIn() {
         return loggingIn;
     }
 
+    /**
+     *
+     * @return the username
+     */
     public MutableLiveData<String> getUsername() {
         return username;
     }
 
+    /**
+     *
+     * @return the password
+     */
     public MutableLiveData<String> getPassword() {
         return password;
     }
 
+    /**
+     *
+     * @return the {@link LoginResult} of the latest login attempt, if applicable
+     */
     public MutableLiveData<Optional<LoginResult>> getLoginResult() {
         return loginResult;
     }
 
+    /**
+     * Tries to log in
+     */
     public void attemptLogin() {
         setLoggingIn(true);
         executor.execute(() -> {
