e2d9a3f807775b68697f2008e039b359e825b739
bulldf <jmyw139@gmail.com>
2022-11-27 11:46:18 -0500

    Modified tests

--- backend/src/test/java/com/javahelp/backend/endpoint/delete/DeleteHandlerTest.java
+++ backend/src/test/java/com/javahelp/backend/endpoint/delete/DeleteHandlerTest.java
@@ -4,8 +4,10 @@ package com.javahelp.backend.endpoint.delete;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assume.assumeTrue;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.request;
+import static io.restassured.RestAssured.when;
 
 import com.javahelp.backend.data.ITokenStore;
 import com.javahelp.backend.data.IUserStore;
@@ -13,8 +15,10 @@ import com.javahelp.model.token.Token;
 import com.javahelp.model.user.ClientUserInfo;
 import com.javahelp.model.user.SHAPasswordHasher;
 import com.javahelp.model.user.User;
+import com.javahelp.model.user.UserInfo;
 import com.javahelp.model.user.UserPassword;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,41 +37,49 @@ public class DeleteHandlerTest {
     final ITokenStore tokenStore = ITokenStore.getDefaultImplementation();
     private static final String USER_ENDPOINT = "https://gwkvm1k2j5.execute-api.us-east-1.amazonaws.com/users/";
 
-    ClientUserInfo userInfo;
-    UserPassword password;
-    User user;
-    Token token;
-
-    @Before
-    public void setUp() {
-        userInfo = new ClientUserInfo("123@mail.com", "University of Toronto",
-                "123-456-7890", "J", "M");
-
-        password = new UserPassword("password", SHAPasswordHasher.getInstance());
-        user = new User("test", userInfo, "asdfgh");
-        userStore.create(user, password);
-        token = new Token(Duration.ofMinutes(30), "test", user.getStringID());
-        tokenStore.create(token);
+    /**
+     * @return whether the database is accessible from the current process
+     */
+    public boolean databaseAccessible() {
+        try {
+            userStore.read("test");
+            tokenStore.read("test");
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Test
     public void testSuccess() {
-        JsonObject body = Json.createObjectBuilder()
-                        .add("userid", user.getStringID())
-                        .add("token", token.getToken())
-                        .build();
+        assumeTrue(databaseAccessible());
 
-        given().header(new Header("Content-Type", "application/json"))
-                .header(new Header("Authorization", "JavaHelp id=" + user.getStringID()
-                + " token=" + token.getToken()))
-                .when().delete(USER_ENDPOINT + user.getStringID()).then().statusCode(400)
-                        .body("message", equalTo("Must include user id and token " +
-                                "in authorization header"));
-    }
+        UserInfo userInfo = new ClientUserInfo("123@mail.com", "University of Toronto",
+                "123-456-7890", "J", "M");
 
-    @After
-    public void cleanUp() {
-        tokenStore.delete(token.getToken());
-        userStore.delete(user.getStringID());
+        UserPassword password = new UserPassword("password", SHAPasswordHasher.getInstance());
+        User user = new User("test", userInfo, "asdfgh");
+        Token token = new Token(Duration.ofMinutes(30), "test", user.getStringID());
+
+        try {
+            userStore.create(user, password);
+            tokenStore.create(token);
+
+
+            JsonObject body = Json.createObjectBuilder()
+                    .add("userid", user.getStringID())
+                    .add("token", token.getToken())
+                    .build();
+
+            given().header(new Header("Content-Type", "application/json"))
+                    .header(new Header("Authorization", "JavaHelp id=" + user.getStringID()
+                            + " token=" + token.getToken()))
+                    .when().delete(USER_ENDPOINT + user.getStringID()).then().statusCode(200)
+                    .body("success", equalTo(true));
+
+        } finally {
+            tokenStore.delete(token.getToken());
+            userStore.delete(user.getStringID());
+        }
     }
 }
