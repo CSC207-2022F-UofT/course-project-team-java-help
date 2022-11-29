489a74075cca9ec7d8db862116e38f84c656f6fb
stevolopolis <stevenluots@gmail.com>
2022-11-29 18:13:30 -0500

    added interface for queryConstraint

---
+++ backend/src/main/java/com/javahelp/backend/query/IUserQueryConstraint.java
@@ -0,0 +1,25 @@
+package com.javahelp.backend.query;
+
+import com.javahelp.model.survey.SurveyResponse;
+import com.javahelp.model.user.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface IUserQueryConstraint {
+    /**
+     *
+     * @param constraints {@link List} of {@link Constraint}s to be applied for query.
+     * @return {@link Set} of Users that satisfy the list of {@link Constraint}s.
+     */
+    Map<String, User> getProvidersWithConstraints(List<Constraint> constraints);
+
+    /**
+     *
+     * @param constraints{ @link List} of {@link Constraint}s to be applied for query.
+     * @return {@link Map} of {@link String} ID of users and their corresponding
+     * {@link SurveyResponse}s.
+     */
+    Map<String, SurveyResponse> getResponsesWithConstraints(List<Constraint> constraints);
+}
