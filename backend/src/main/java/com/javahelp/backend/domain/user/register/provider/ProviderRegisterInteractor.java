31ebfbe16d93f35240c9a95ae7d6d67e72917a9f
Lam207 <113150246+Lam207@users.noreply.github.com>
2022-12-01 09:28:48 -0500

    implement UserRegisterInteractor for providers

---
+++ backend/src/main/java/com/javahelp/backend/domain/user/register/provider/ProviderRegisterInteractor.java
@@ -0,0 +1,36 @@
+package com.javahelp.backend.domain.user.register.provider;
+
+import com.javahelp.backend.data.IUserStore;
+import com.javahelp.backend.domain.user.register.UserRegisterResult;
+import com.javahelp.backend.domain.user.register.UserRegisterInteractor;
+import com.javahelp.model.user.ProviderUserInfo;
+import com.javahelp.model.user.User;
+import com.javahelp.model.user.UserPassword;
+
+/**
+ * Interactor that registers new provider {@link User}s
+ */
+public class ProviderRegisterInteractor extends
+        UserRegisterInteractor<IProviderRegisterInputBoundary, ProviderUserInfo> {
+
+    /**
+     * Creates a new {@link ProviderRegisterInteractor} instance
+     * @param userStore {@link IUserStore} to use
+     */
+    public ProviderRegisterInteractor(IUserStore userStore) {
+        super(userStore);
+    }
+
+    @Override
+    protected ProviderUserInfo bundleUserInfo(IProviderRegisterInputBoundary boundary) {
+        ProviderUserInfo p = new ProviderUserInfo(boundary.getEmailAddress(),
+                boundary.getAddress(), boundary.getPhoneNumber(), boundary.getPracticeName());
+        p.setCertified(boundary.getCertified());
+        return p;
+    }
+
+    @Override
+    protected UserRegisterResult isValidated(IProviderRegisterInputBoundary boundary) {
+        return null;
+    }
+}
