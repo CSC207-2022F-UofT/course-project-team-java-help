675fd25d243c47534aa6ceec6254b4b02cac9e66
stevolopolis <stevenluots@gmail.com>
2022-12-02 02:51:38 -0500

    initialized interface for providerRanker

--- backend/src/main/java/com/javahelp/backend/search/rank/IProviderRanker.java
+++ backend/src/main/java/com/javahelp/backend/search/rank/IProviderRanker.java
@@ -1,15 +1,29 @@
 package com.javahelp.backend.search.rank;
 
+import com.javahelp.model.survey.SurveyResponse;
 import com.javahelp.model.user.User;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public interface IProviderRanker {
-    List<User> rank(Set<User> userList);
+    /**
+     *
+     * @param responseMain {@link SurveyResponse} to be compared with (i.e. the client)
+     * @param responses {@link Map} of {@link User}s and their {@link SurveyResponse}s to be ranked
+     * @return {@link List} of {@link User}s ranked in descending order of similarity
+     * with the main user.
+     */
+    List<User> rank(SurveyResponse responseMain, Map<User, SurveyResponse> responses);
 
-    Map<User, Float> getUsersWithScores(User userMain, Set<User> userList);
-
-    List<Float> getSortedScoreList(Map<User, Float> userScores);
+    /**
+     *
+     * @param mainResponse {@link SurveyResponse} of {@link User} to be ranked
+     * @param responses {@link Map} of {@link User}s and their {@link SurveyResponse}s
+     *                            to be compared with.
+     * @return {@link Map} of {@link User}s with their corresponding similarity scores
+     * with userMain.
+     */
+    Map<User, Float> getUsersWithScores(SurveyResponse mainResponse,
+                                        Map<User, SurveyResponse> responses);
 }
