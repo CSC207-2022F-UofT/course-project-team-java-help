b59b822af169951e15af53acf33ff0c295512f3d
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-12-02 22:58:43 -0500

    add UserReadInteractor

---
+++ backend/src/main/java/com/javahelp/backend/domain/user/read/UserReadInteractor.java
@@ -0,0 +1,30 @@
+package com.javahelp.backend.domain.user.read;
+
+import com.javahelp.backend.data.IUserStore;
+import com.javahelp.model.user.User;
+
+/**
+ * Interactor for reading a {@link User} and passing back their info
+ */
+public class UserReadInteractor {
+
+    private final IUserStore users;
+
+    /**
+     * *
+     * @param users
+     */
+    public UserReadInteractor(IUserStore users) {
+        this.users = users;
+    }
+
+    /**
+     * Returns the {@link User} with the specified id, or null if not found
+     * @param input {@link IUserReadInput} containing information about the specified {@link User}
+     * @return the {@link User} or null
+     */
+    public User readUser(IUserReadInput input) {
+        return users.read(input.getID());
+    }
+
+}
