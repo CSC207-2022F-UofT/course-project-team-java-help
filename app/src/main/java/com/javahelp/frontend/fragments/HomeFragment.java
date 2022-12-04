2166b6ebb6cdd637866f11ea62b98de27aa509da
zul22 <zulaa.lkhagvajav@mail.utoronto.ca>
2022-12-04 14:46:29 -0500

    Change Login background and adjust some parts.

--- app/src/main/java/com/javahelp/frontend/fragments/HomeFragment.java
+++ app/src/main/java/com/javahelp/frontend/fragments/HomeFragment.java
@@ -16,41 +16,18 @@ import com.javahelp.R;
  */
 public class HomeFragment extends Fragment {
 
-    // This is example code. These will be changed later
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
     public HomeFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
