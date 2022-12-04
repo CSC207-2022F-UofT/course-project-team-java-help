850aff4a31b767adb852a0ee077fc56f03b88af1
stevolopolis <stevenluots@gmail.com>
2022-12-03 22:33:13 -0500

    minor fixes.

--- backend/src/main/java/com/javahelp/backend/search/SearchInteractor.java
+++ backend/src/main/java/com/javahelp/backend/search/SearchInteractor.java
@@ -3,6 +3,7 @@ package com.javahelp.backend.search;
 import com.javahelp.backend.data.ISurveyResponseStore;
 import com.javahelp.backend.data.ISurveyStore;
 import com.javahelp.backend.data.IUserStore;
+import com.javahelp.backend.search.constraint.IConstraint;
 import com.javahelp.backend.search.constraint.UserQueryConstraint;
 import com.javahelp.backend.search.rank.IProviderRanker;
 import com.javahelp.backend.search.rank.ISimilarityScorer;
@@ -30,11 +31,14 @@ public class SearchInteractor {
     }
 
     public SearchResult search(ISearchInput input) {
-        SurveyResponse responseMain = this.responseStore.readByUser(input.getUserID()).get(0);
+        IConstraint constraint = IConstraint.getDefaultImplementation();
+        for (String c : input.getConstraints()) {
+            constraint.setConstraint(c);
+        }
 
         UserQueryConstraint userQueryConstraint = new UserQueryConstraint(this.responseStore, this.userStore);
-        Map<String, User> users = userQueryConstraint.getProvidersWithConstraints(input.getConstraint());
-        Map<String, SurveyResponse> responses = userQueryConstraint.getResponsesWithConstraints(input.getConstraint());
+        Map<String, User> users = userQueryConstraint.getProvidersWithConstraints(constraint);
+        Map<String, SurveyResponse> responses = userQueryConstraint.getResponsesWithConstraints(constraint);
 
         List<User> userList = new ArrayList<>();
         List<SurveyResponse> responseList = new ArrayList<>();
@@ -45,7 +49,8 @@ public class SearchInteractor {
             userToResponseMap.put(users.get(userID), responses.get(userID));
         }
 
-        if (input.getIsRanking()) {
+        if (input.getIsRanking() && input.getUserID() != null && input.getUserID() != "null") {
+            SurveyResponse responseMain = this.responseStore.readByUser(input.getUserID()).get(0);
             ISimilarityScorer similarityScorer = new SimilarityScorer();
             IProviderRanker ranker = new ProviderRanker(similarityScorer);
             userList = ranker.rank(responseMain, userToResponseMap);
